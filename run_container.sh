63bb0da65a7f70906d61d68c55af7bc779d84d21
Hiromichi Iizuka <iizuka@eduam.co.jp>
2020-11-22 17:54:30 +0900

    Add default options

--- run_container.sh
+++ run_container.sh
@@ -1,5 +1,11 @@
-if [ $1 == "start" ]; then
-    docker run -d --rm -it --name ppcnn ppcnn
-elif [ $1 == "stop" ]; then
+if [ $# -ge 1 ]; then
+    cmd=$1
+else
+    cmd="start"
+fi
+
+if [ $cmd == "start" ]; then
+    docker run -d --rm -it -v `pwd`:/root/work/PP-CNN --name ppcnn ppcnn
+elif [ $cmd == "stop" ]; then
     docker stop ppcnn
 fi
